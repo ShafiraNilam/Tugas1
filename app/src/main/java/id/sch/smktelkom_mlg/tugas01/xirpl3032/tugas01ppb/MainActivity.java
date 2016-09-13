f663fd95fd50af93e30385c44ff47a77262f2bd5
Shafira Nilamsari M <ns369shafira@gmail.com>
2016-09-13 19:05:47 +0700

    membuat pilihan di spinner

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3032/tugas01ppb/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3032/tugas01ppb/MainActivity.java
@@ -6,19 +6,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
-    EditText etNama;
-    EditText etUmur;
-    Button bOk;
-    CheckBox cb1;
-    CheckBox cb2;
-    CheckBox cb3;
-    CheckBox cb4;
-    CheckBox cb5;
+    EditText etNama, etUmur;
+    Button buttonOk;
+    CheckBox cb1, cb2, cb3, cb4, cb5;
     TextView tvHasil;
+    Spinner spinnerAsal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,18 +24,35 @@ public class MainActivity extends AppCompatActivity
 
         etNama = (EditText) findViewById(R.id.editTextNama);
         etUmur = (EditText) findViewById(R.id.editTextUmur);
-        bOk = (Button) findViewById(R.id.buttonOk);
-        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        cb1 = (CheckBox) findViewById(R.id.cb1);
+        cb2 = (CheckBox) findViewById(R.id.cb2);
+        cb3 = (CheckBox) findViewById(R.id.cb3);
+        cb4 = (CheckBox) findViewById(R.id.cb4);
+        cb5 = (CheckBox) findViewById(R.id.cb5);
+        spinnerAsal = (Spinner) findViewById(R.id.spinnerAsal);
+        buttonOk = (Button) findViewById(R.id.buttonOk);
 
-        bOk.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.buttonOk).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String nama = etNama.getText().toString();
-                int usia = Integer.parseInt(etUmur.getText().toString());
-                tvHasil.setText("Terima Kasih" + nama + "Telah memilih" +);
+            public void onClick(View view) {
+                doClick();
             }
         });
+
+
     }
 
+    private void doClick() {
+        String hasil = "Terima Kasih " + etNama.getText().toString() + " Telah memilih \n";
 
+        int startlen = hasil.length();
+        if (cb1.isChecked()) hasil += cb1.getText() + "\n";
+        if (cb2.isChecked()) hasil += cb2.getText() + "\n";
+        if (cb3.isChecked()) hasil += cb3.getText() + "\n";
+        if (cb4.isChecked()) hasil += cb4.getText() + "\n";
+        if (cb5.isChecked()) hasil += cb5.getText() + "\n";
+        if (hasil.length() == startlen) hasil += "Tidak ada pilihan";
+
+        tvHasil.setText(hasil);
+    }
 }
