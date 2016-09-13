18ac04c963a5c7227bef07f42fe13427edb280a2
Shafira Nilamsari M <ns369shafira@gmail.com>
2016-09-13 12:17:58 +0700

    mengubah desain inteface ke scroll view

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3032/tugas01ppb/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3032/tugas01ppb/MainActivity.java
@@ -2,12 +2,25 @@ package id.sch.smktelkom_mlg.tugas01.xirpl3032.tugas01ppb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
+    EditText etNama;
+    EditText etTahun;
+    Button bOk;
+    TextView tvHasil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etTahun = (EditText) findViewById(R.id.editTextUmur);
+        bOk = (Button) findViewById(R.id.buttonOk);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
     }
 }
