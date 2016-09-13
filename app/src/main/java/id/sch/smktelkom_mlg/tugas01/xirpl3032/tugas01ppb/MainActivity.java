496b4093951d6c6d7ad52f311e0a9dd522631037
Shafira Nilamsari M <ns369shafira@gmail.com>
2016-09-13 15:13:39 +0700

    membuat value xml baru

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3032/tugas01ppb/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3032/tugas01ppb/MainActivity.java
@@ -1,26 +1,44 @@
 package id.sch.smktelkom_mlg.tugas01.xirpl3032.tugas01ppb;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
     EditText etNama;
-    EditText etTahun;
+    EditText etUmur;
     Button bOk;
+    CheckBox cb1;
+    CheckBox cb2;
+    CheckBox cb3;
+    CheckBox cb4;
+    CheckBox cb5;
     TextView tvHasil;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         etNama = (EditText) findViewById(R.id.editTextNama);
-        etTahun = (EditText) findViewById(R.id.editTextUmur);
+        etUmur = (EditText) findViewById(R.id.editTextUmur);
         bOk = (Button) findViewById(R.id.buttonOk);
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
 
+        bOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nama = etNama.getText().toString();
+                int usia = Integer.parseInt(etUmur.getText().toString());
+                tvHasil.setText("Terima Kasih" + nama + "Telah memilih" +);
+            }
+        });
     }
+
+
 }
